114eab98377e484a3839d09fa701e2037211f1a2
nishinoshungo <nishishun50@gmail.com>
2021-07-13 03:46:22 +0000

    add mypage

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9d11e036a3013865001ce53b3b8a4d68d3a4c739aa726dbc66586035cde406c2deed47ee488b933d310b1c64b65f74ab0914dfc0d63dc8e26b2243dba7ed6bdc'
